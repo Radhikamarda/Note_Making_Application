6fcafe14b43072ed8950e20b9fe5e1045499040c
Radhikamarda <124992977+Radhikamarda@users.noreply.github.com>
2023-07-26 21:39:53 +0530

    Add files via upload

---
+++ Note Making Application/src/components/Notes.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1> This is front end web development bootcamp </h1>
+      <p>
+        {" "}
+        Thankyou DevTown ,Microsoft and Google For providing Certificate
+        ThankYou Shaurya Sinha Sir for teaching us basics of HTML ,CSS and JS.
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
